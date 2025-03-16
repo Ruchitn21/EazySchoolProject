72ccfb056d5e180919d45855869f5c3bc4d19308
Ruchit Nigam <ruchitnigam@Ruchits-MacBook-Air.local>
2025-03-16 15:20:00 +0530

    Intial commit for UI and basic backend

---
+++ src/test/java/com/helloworld/HelloWordProject/HelloWordProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.HelloWordProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
