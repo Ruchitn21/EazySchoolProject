1e8e7bb569e0b091d64600de02b8dfb77ec48d7f
Ruchit Nigam <nigam21ruchit@gmail.com>
2025-03-20 17:36:54 +0530

    [OBIT-12345] - added contact controller and contact POJO class
    Co-authored-by: Ruchit Nigam <ruchitnigam@Ruchits-MacBook-Air.local>

---
+++ src/main/java/com/helloworld/HelloWordProject/controller/ContactController.java
@@ -0,0 +1,42 @@
+package com.helloworld.HelloWordProject.controller;
+
+import com.helloworld.HelloWordProject.model.Contact;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@Controller
+public class ContactController {
+
+    private final Logger logger = LoggerFactory.getLogger(ContactController.class);
+
+    @RequestMapping("/contact")
+    public String getContactPage() {
+        return "contact";
+    }
+
+//    @RequestMapping(value = "/saveMsg", method = POST)
+//    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,
+//                                    @RequestParam String email, @RequestParam String subject,
+//                                    @RequestParam String message) {
+//        logger.info("Name : {}",name);
+//        logger.info("Mobile Number : {}", mobileNum);
+//        logger.info("Email : {}", email);
+//        logger.info("Subject : {}", subject);
+//        logger.info("Message : {}", message);
+//
+//        return new ModelAndView("redirect:/contact");
+//
+//    }
+
+    @RequestMapping(value = "/saveMsg", method = POST)
+    public ModelAndView saveMessage(Contact contact) {
+
+        return new ModelAndView("redirect:/contact");
+
+    }
+}
