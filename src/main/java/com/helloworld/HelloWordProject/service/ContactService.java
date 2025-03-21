2ac83496398f344eaa63956a23f1aaf7e93aae05
ruchitnigam <nigam21ruchit@gmail.com>
2025-03-21 08:47:44 +0530

    [OBIT-12345] - added path variable logic for holidays and added @SLF4J annotation

--- src/main/java/com/helloworld/HelloWordProject/service/ContactService.java
+++ src/main/java/com/helloworld/HelloWordProject/service/ContactService.java
@@ -1,21 +1,23 @@
 package com.helloworld.HelloWordProject.service;
 
 import com.helloworld.HelloWordProject.model.Contact;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ContactService {
 
-    Logger logger = LoggerFactory.getLogger(ContactService.class);
+//    Logger logger = LoggerFactory.getLogger(ContactService.class); not required because of @Slf4j annotation
 
     public boolean saveMessageDetails(Contact contact) {
 
         boolean isSaved = true;
 
         //TODO - need to persist data in the DB
-        logger.info(contact.toString());
+        log.info(contact.toString());
         return isSaved;
     }
 }
