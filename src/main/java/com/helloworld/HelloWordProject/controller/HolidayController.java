50f81bdc13d9bb1bb7ebcfea206f2409966e9a4d
ruchitnigam <nigam21ruchit@gmail.com>
2025-03-22 09:42:28 +0530

    [OBIT-12345] - added security and added field level validations for contact page

--- src/main/java/com/helloworld/HelloWordProject/controller/HolidayController.java
+++ src/main/java/com/helloworld/HelloWordProject/controller/HolidayController.java
@@ -4,6 +4,8 @@ import com.helloworld.HelloWordProject.model.Holiday;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
